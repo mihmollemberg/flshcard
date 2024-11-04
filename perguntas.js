37a90651cca7f4b0a643b034f7123e31ceeab563
mihmollemberg <00001095655681sp@al.educacao.sp.gov.br>
2024-11-04 10:48:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual a célula reprodura masculina',
+    'O espermatozóide'
+)
+
+criaCartao(
+    'Redes sociais',
+    'Qual a maior influenciadora brasileira?',
+    'Vírginia Fonseca'
+)
+
+criaCartao(
+    'Tecnologia',
+    'quais comandos são utilizados para copiar e colar?',
+    'Ctrl c e Ctrl v'
+)
+
+criaCartao(
+    'culinária',
+    'Qual o prato Típico do Brasil?',
+    'Feijoada'
+)
